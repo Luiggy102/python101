70468f2f5f4eab0a5470c6d3ec15036d7c0973c6
Luiggy102 <work_luiggyn.h5d9a@slmail.me>
2023-04-15 18:12:51 -0500

    bool file completed

---
+++ 5bool.py
@@ -0,0 +1,9 @@
+esta_soltero = True
+print(f"Esta soltero el usuario: {esta_soltero}\n el tipo de dato es: {type(esta_soltero)}")
+
+esta_soltero = not True
+print(f"\n Esta soltero el usuario: {esta_soltero}\n el tipo de dato es: {type(esta_soltero)}")
+
+esta_soltero = not esta_soltero
+print(f"\n Esta soltero el usuario: {esta_soltero}\n el tipo de dato es: {type(esta_soltero)}")
+print("Estado invertido con `not`")
