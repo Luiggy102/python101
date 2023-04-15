52c0feab45214d388a331d779a75157d06b91a71
Luiggy102 <work_luiggyn.h5d9a@slmail.me>
2023-04-15 17:47:27 -0500

    nums

---
+++ 4nums.py
@@ -0,0 +1,18 @@
+vidas = 3
+# print(type(vidas)) # int
+
+presupuesto = 100 
+prestamo = 300
+
+print(f"\n presupuesto: {presupuesto}, prestamo: {prestamo} \n y el tipo de dato de los dos sumados es {type(prestamo + presupuesto)} \n")
+
+temmperatura = 28.6 
+print(f"La temmperatura es {temmperatura}, y el tipo de dato es {type(temmperatura)} \n")
+
+vidas = vidas - 1
+vidas -= 1
+print(f"Numero de vidas: {vidas}")
+vidas -= 5
+print(f"Numero de vidas: {vidas}, el tipo de dato es {type(vidas)}")
+
+pi = 3.14159265358979323846
