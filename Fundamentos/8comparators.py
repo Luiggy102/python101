72f8c510db1dbb1abddc0ead183956cd5f02b9b6
Luiggy102 <work_luiggyn.h5d9a@slmail.me>
2023-04-27 10:26:55 -0500

    comparators file

---
+++ Fundamentos/8comparators.py
@@ -0,0 +1,18 @@
+# comparacion 
+print(7 > 3)
+print(7 < 3)
+
+# doble comparacion
+print(5 <= 4)
+print(2 >= 2)
+
+# igualdad
+print(6 == 6)
+print(5 == 2)
+
+# diferente de
+print(6 != 10)
+print(6 != 6)
+
+print("Apple" == 'Apple')
+print("Apple" == 'apple')
