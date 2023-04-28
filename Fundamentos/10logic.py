5a191b9e02082b6a9e8c47e1a579013b9e5b7d6f
Luiggy102 <work_luiggyn.h5d9a@slmail.me>
2023-04-28 11:25:19 -0500

    logic file

---
+++ Fundamentos/10logic.py
@@ -0,0 +1,11 @@
+# and
+
+num = int(input('Digite un número número de stock admitido por la empresa => '))
+
+print(num >= 100 and num <= 1000)
+
+# or
+
+role = input('Coloque su rol en la empresa => ')
+
+print(role == 'admin' or role == 'soller')
